383c6f5d5893f38f9dc5d4bbca7b14dd78776e3a
Ahmed Magdy Ahmed <Ahmed.magdy.2788@gmail.com>
2025-12-23 20:12:17 +0200

    feat: Implement Discount Microservice with gRPC, database integration, and CRUD operations

--- src/Services/Discount/Discount.Grpc/Models/Coupon.cs
+++ src/Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -2,5 +2,8 @@ namespace Discount.Grpc.Models;
 
 public class Coupon
 {
-    
+    public int Id { get; set; }
+    public required string ProductName { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public int Amount { get; set; }
 }
