93fdceb72cde8e29e2576f554e70682cd5c46a69
Ahmed Magdy Ahmed <Ahmed.magdy.2788@gmail.com>
2025-12-27 16:37:21 +0200

    feat: Add Ordering microservice with initial structure and project files

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -42,6 +42,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
